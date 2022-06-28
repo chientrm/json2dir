27648b0f184ed866baae310a467b5f582e458c28
chientrm <chientrm@gmail.com>
2022-06-28 11:39:06 +0000

    feat: json2dir

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { writeFileSync, mkdirSync } from 'fs'
+
+const json2dir = (
+    dir: string,
+    serializers: Record<string, (obj: any) => string>,
+    obj: any
+) => {
+    mkdirSync(dir, { recursive: true })
+    Object.entries(obj).forEach(([key, value]) => {
+        if (!value) return
+        const a = key.split('.')
+        a.shift()
+        const ext = a.join('.')
+        if (ext) {
+            const serializer = serializers[ext]
+            if (!serializer) throw new Error(`No serializer for ext '${key}'`)
+            writeFileSync(`${dir}/${key}`, serializer(value))
+        } else json2dir(`${dir}/${key}`, serializers, value)
+    })
+}
+
+export default json2dir
---
+++ test/index.test.js
@@ -0,0 +1,28 @@
+const json2dir = require('../dist').default
+const { rmSync, readFileSync } = require('fs')
+const ini = require('ini')
+
+test('json2dir', () => {
+    const dir = '.test'
+    rmSync(dir, { recursive: true, force: true })
+    const bob = { name: 'Bob', age: 23 }
+    json2dir(
+        dir,
+        {
+            ini: ini.encode,
+            json: JSON.stringify
+        },
+        {
+            'a.json': bob,
+            'b.ini': { INFO: bob },
+            c: {
+                d: {
+                    'e.json': bob
+                }
+            }
+        }
+    )
+    expect(readFileSync(`${dir}/a.json`, 'utf8')).toBe(JSON.stringify(bob))
+    expect(readFileSync(`${dir}/b.ini`, 'utf8')).toBe(ini.encode({ INFO: bob }))
+    expect(readFileSync(`${dir}/c/d/e.json`, 'utf8')).toBe(JSON.stringify(bob))
+})
