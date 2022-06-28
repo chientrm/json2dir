700a65f2fa45876b289fa4b1264f9f67b4208a13
chientrm <chientrm@gmail.com>
2022-06-28 12:33:55 +0000

    fix

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+declare const json2dir: (dir: string, serializers: Record<string, (obj: any) => string>, obj: any) => void;
+export default json2dir;
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 import json2dir from "json2dir";
-console.log(json2dir)
 
 json2dir('.test', { json: JSON.stringify }, { 'a.json': { name: 'bob' } })
